c2f459c5dec79b2c7e8bb0c4dff8f6d0a00b0b71
Ali Abou-Zahr <ali.a.zahr@gmail.com>
2015-04-09 02:00:29 -0700

    stripe accepting custom payment amounts

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,10 @@
 class ChargesController < ApplicationController
-  
+
   def new; end
 
   def create
     # Amount in cents
-    @amount = 500
+    @amount = params[:amount]
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
